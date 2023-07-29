08380628d5a1962660e752b6774b1c59e9768dec
Jeston Lewis <jeston.lewis@protonmail.com>
2023-07-29 14:24:13 -0500

    moved roster from cpp to h

--- JYM1-ClassRoster/roster.h
+++ JYM1-ClassRoster/roster.h
@@ -1,1 +1,18 @@
-#include <iostream>
+#ifndef ROSTER_H
+#define ROSTER_H
+
+#include "student.h"
+
+using namespace std;
+
+class Roster {
+public:
+    void add();
+    void remove();
+    void printAll();
+    void printAverageDaysInCourse();
+    void printInvalidEmails();
+    void printByDegreeProgram();
+};
+
+#endif
