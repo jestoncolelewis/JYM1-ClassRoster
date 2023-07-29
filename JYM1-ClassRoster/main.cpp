984729fb5199edc3b57b27840b5847730077be8d
Jeston Lewis <jeston.lewis@protonmail.com>
2023-07-29 14:23:41 -0500

    added Roster initialization and studentdata

--- JYM1-ClassRoster/main.cpp
+++ JYM1-ClassRoster/main.cpp
@@ -6,9 +6,15 @@
 //
 
 #include <iostream>
+#include "roster.h"
+#include "student.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    Roster classRoster;
+    
+    const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Jeston,Lewis,jlew910@wgu.edu,34,[numberofdaystocomplete3courses],SOFTWARE"};
+    
     return 0;
 }
