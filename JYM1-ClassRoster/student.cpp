1119ad67ddb2b9ce7c6ba3656d9e1d74032030c1
Jeston Lewis <jeston.lewis@protonmail.com>
2023-07-29 14:24:50 -0500

    moved student from cpp to h

--- JYM1-ClassRoster/student.cpp
+++ JYM1-ClassRoster/student.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-class Student {
-    int ID;
-    string firstName;
-    string lastName;
-    string email;
-    int age;
-    int daysToComplete[];
-};
+#include "student.h"
---
+++ JYM1-ClassRoster/student.h
@@ -0,0 +1,19 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+#include "degree.h"
+
+using namespace std;
+
+class Student {
+    int ID;
+    string firstName;
+    string lastName;
+    string email;
+    int age;
+    int daysToComplete[];
+};
+
+#endif
