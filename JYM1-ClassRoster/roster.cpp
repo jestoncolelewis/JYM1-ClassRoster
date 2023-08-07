468c401aa894ea8fd92ba5f768693b7010ad4019
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-07 14:22:52 -0600

    built more of the roster class

--- JYM1-ClassRoster/roster.cpp
+++ JYM1-ClassRoster/roster.cpp
@@ -1,1 +1,33 @@
 #include "roster.h"
+
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram) {
+//    Determine what to add
+}
+
+void Roster::remove(string studentID) {
+//    check if student exists
+//    if not print
+    const string errMsg = "No student with ID: " + studentID + " exists.";
+    cout << errMsg << endl;
+//    remove specific student data from array
+}
+
+void Roster::printAll() {
+//    Loop through all students
+//    print student data in a tab separated format
+}
+
+void Roster::printAverageDaysInCourse(string studentID) {
+//    print average of days in Course for a specific student
+}
+
+void Roster::printInvalidEmails() {
+//    verifies student emails
+//    prints message for invalid email addresses
+    const string note = "Note: A valid email should include an at sign ('@') and period ('.') and should not include a space (' ').";
+    cout << note << endl;
+}
+
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram) {
+//    prints student information for a specific degree program
+}
--- JYM1-ClassRoster/roster.h
+++ JYM1-ClassRoster/roster.h
@@ -7,12 +7,15 @@ using namespace std;
 
 class Roster {
 public:
-    void add();
-    void remove();
+    void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeprogram);
+    void remove(string studentID);
     void printAll();
-    void printAverageDaysInCourse();
+    void printAverageDaysInCourse(string studentID);
     void printInvalidEmails();
-    void printByDegreeProgram();
+    void printByDegreeProgram(DegreeProgram degreeProgram);
+    
+private:
+    Student classRosterArray[5];
 };
 
 #endif
