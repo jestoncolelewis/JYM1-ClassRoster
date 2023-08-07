a4f5ec3c88266c64135b591b16e153daecffce8b
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-07 13:59:54 -0600

    initial declaration and definition of Student Class

--- JYM1-ClassRoster/student.cpp
+++ JYM1-ClassRoster/student.cpp
@@ -1,1 +1,67 @@
 #include "student.h"
+
+// Accessors
+string Student::getStudentID() {
+    cout << studentID << endl;
+    return studentID;
+}
+
+string Student::getFName() {
+    cout << firstName << endl;
+    return firstName;
+}
+
+string Student::getLName() {
+    cout << lastName << endl;
+    return lastName;
+}
+
+string Student::getEmail() {
+    cout << email << endl;
+    return email;
+}
+
+int Student::getAge() {
+    cout << age << endl;
+    return age;
+}
+
+DegreeProgram Student::getDegreeProgram() {
+    cout << degreeProgram << endl;
+    return degreeProgram;
+}
+
+int* Student::getDaysToComplete() {
+    cout << daysToComplete << endl;
+    return daysToComplete;
+}
+
+
+// Mutators
+void Student::editStudentID(string in) {
+    studentID = in;
+}
+
+void Student::editFName(string in) {
+    firstName = in;
+}
+
+void Student::editLName(string in) {
+    lastName = in;
+}
+
+void Student::editEmail(string in) {
+    email = in;
+}
+
+void Student::editAge(int in) {
+    age = in;
+}
+
+void Student::editDegreeProgram(DegreeProgram in) {
+    degreeProgram = in;
+}
+
+void Student::editDaysToComplete(int in) {
+    daysToComplete[0] = in;
+} // I have a feeling this is wrong
--- JYM1-ClassRoster/student.h
+++ JYM1-ClassRoster/student.h
@@ -8,11 +8,31 @@
 using namespace std;
 
 class Student {
-    int ID;
+public:
+    // Accessors
+    string getStudentID();
+    string getFName();
+    string getLName();
+    string getEmail();
+    int getAge();
+    DegreeProgram getDegreeProgram();
+    int* getDaysToComplete();
+    
+    // Mutators
+    void editStudentID(string in);
+    void editFName(string in);
+    void editLName(string in);
+    void editEmail(string in);
+    void editAge(int in);
+    void editDegreeProgram(DegreeProgram in);
+    void editDaysToComplete(int in);
+private:
+    string studentID;
     string firstName;
     string lastName;
     string email;
     int age;
+    DegreeProgram degreeProgram;
     int daysToComplete[];
 };
 
