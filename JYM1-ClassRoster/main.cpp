3f06aa932ef33a158df7f9b073a4fe82b7957d7a
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-09 16:07:56 -0600

    removed DegreeProgram initialization

--- JYM1-ClassRoster/main.cpp
+++ JYM1-ClassRoster/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int main() {
     Roster classRoster;
-    DegreeProgram degree {SOFTWARE};
+    DegreeProgram degree;
     
     const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Jeston,Lewis,jlew910@wgu.edu,34,15,50,28,SOFTWARE"};
     
@@ -43,7 +43,7 @@ int main() {
     
     classRoster.printInvalidEmails();
     
-    classRoster.printAverageDaysInCourse(<#string studentID#>);
+//    classRoster.printAverageDaysInCourse(<#string studentID#>);
 //    loop Roster and do ^^
     
     classRoster.printByDegreeProgram(SOFTWARE);
