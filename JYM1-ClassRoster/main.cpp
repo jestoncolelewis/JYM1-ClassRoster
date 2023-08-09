7a7491499b8ddc319a158db2a936c6bb7ac16dd5
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-09 15:50:22 -0600

    added degreeProgram type and passed it to the classRoster

--- JYM1-ClassRoster/main.cpp
+++ JYM1-ClassRoster/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 int main() {
     Roster classRoster;
+    DegreeProgram degree {SOFTWARE};
     
     const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Jeston,Lewis,jlew910@wgu.edu,34,15,50,28,SOFTWARE"};
     
@@ -26,7 +27,16 @@ int main() {
             singleStudent[j] = studentData[i].substr(0, pos);
             ++j;
         }
-        classRoster.add(singleStudent[0], singleStudent[1], singleStudent[2], singleStudent[3], stoi(singleStudent[4]), stoi(singleStudent[5]), stoi(singleStudent[6]), stoi(singleStudent[7]), <#DegreeProgram degreeprogram#>);
+        
+        if (singleStudent[8] == "SOFTWARE") {
+            degree = SOFTWARE;
+        } else if (singleStudent[8] == "SECURITY") {
+            degree = SECURITY;
+        } else if (singleStudent[8] == "NETWORK") {
+            degree = NETWORK;
+        };
+            
+        classRoster.add(singleStudent[0], singleStudent[1], singleStudent[2], singleStudent[3], stoi(singleStudent[4]), stoi(singleStudent[5]), stoi(singleStudent[6]), stoi(singleStudent[7]), degree);
     }
     
     classRoster.printAll();
