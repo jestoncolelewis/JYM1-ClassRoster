9ed8f0d2382c671df6c92d73255ae018030bda5e
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-08 20:13:32 -0600

    added loop for sending data to the add function

--- JYM1-ClassRoster/main.cpp
+++ JYM1-ClassRoster/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <string>
 #include "roster.h"
 #include "student.h"
 
@@ -14,7 +15,34 @@ using namespace std;
 int main() {
     Roster classRoster;
     
-    const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Jeston,Lewis,jlew910@wgu.edu,34,15,50,28ç,SOFTWARE"};
+    const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Jeston,Lewis,jlew910@wgu.edu,34,15,50,28,SOFTWARE"};
+    
+    for (int i = 0; i < 5 ; ++i) {
+        string singleStudent[9];
+        size_t pos = 0;
+        string delimiter = ",";
+        int j = 0;
+        while((pos = studentData[i].find(delimiter))) {
+            singleStudent[j] = studentData[i].substr(0, pos);
+            ++j;
+        }
+        classRoster.add(singleStudent[0], singleStudent[1], singleStudent[2], singleStudent[3], stoi(singleStudent[4]), stoi(singleStudent[5]), stoi(singleStudent[6]), stoi(singleStudent[7]), <#DegreeProgram degreeprogram#>);
+    }
+    
+    classRoster.printAll();
+    
+    classRoster.printInvalidEmails();
+    
+    classRoster.printAverageDaysInCourse(<#string studentID#>);
+//    loop Roster and do ^^
+    
+    classRoster.printByDegreeProgram(SOFTWARE);
+    
+    classRoster.remove("A3");
+    
+    classRoster.printAll();
+    
+    classRoster.remove("A3");
     
     return 0;
 }
