44c2058a3c558930ae11988ac7efa682f320e02d
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-10 17:58:07 -0600

    formatting

--- JYM1-ClassRoster/main.cpp
+++ JYM1-ClassRoster/main.cpp
@@ -21,7 +21,13 @@ int main() {
     Roster classRoster;
     DegreeProgram degree {SOFTWARE};
     
-    const string studentData[] = {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Jeston,Lewis,jlew910@wgu.edu,34,15,50,28,SOFTWARE"};
+    const string studentData[] = {
+        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Jeston,Lewis,jlew910@wgu.edu,34,15,50,28,SOFTWARE"
+    };
     
     for (int i = 0; i < 5; ++i) {
         string* singleStudent = classRoster.parse(studentData[i]);
